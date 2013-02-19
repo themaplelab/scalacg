b53ad3dce04aa9f807e9d2bca60ea324bb204aac
olhotak <olhotak@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-19 20:19:22 +0000

    add test for path-dependent types
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10063 e628a048-d915-0410-9aa0-f7087ab87138

--- src/test/Suite.scala
+++ src/test/Suite.scala
@@ -15,12 +15,14 @@ class Suite {
   @Test def testTest1 = runTest("Test1")
   @Test def testTest2 = runTest("Test2")
   @Test def testTest3 = runTest("Test3")
+
+  @Test def testTraits1 = runTest("Traits1")
+  @Test def testTraits2 = runTest("Traits2")
+
+  @Test def testOverriding1 = runTest("Overriding1")
+
+  @Test def testClosures1 = runTest("Closures1")
+  @Test def testClosures2 = runTest("Closures2")
   
-   @Test def testTraits1 = runTest("Traits1")
-   @Test def testTraits2 = runTest("Traits2")
-   
-   @Test def testOverriding1 = runTest("Overriding1")
-   
-   @Test def testClosures1 = runTest("Closures1")
-   @Test def testClosures2 = runTest("Closures2")
+  @Test def testPathTypes1 = runTest("PathTypes1") 
 }
