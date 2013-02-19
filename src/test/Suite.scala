5d914cbc39017112c35d7cc142d373a785cbdeca
olhotak <olhotak@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-19 20:19:18 +0000

    fix indentation
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10054 e628a048-d915-0410-9aa0-f7087ab87138

--- src/test/Suite.scala
+++ src/test/Suite.scala
@@ -4,13 +4,13 @@ import org.junit.Test
 import scala.tools.nsc
 
 class Suite {
-   def runTest(filename: String) = {
-      val settings = new nsc.Settings
-      settings.d.value = "tests/classes"
-        settings.plugin.value = List("bin/callgraph-plugin.jar")
-      val g = new nsc.Global(settings)
-      new (g.Run).compile(List("tests/"+filename+".scala", "tests/target.scala"))
+  def runTest(filename: String) = {
+    val settings = new nsc.Settings
+    settings.d.value = "tests/classes"
+    settings.plugin.value = List("bin/callgraph-plugin.jar")
+    val g = new nsc.Global(settings)
+    new (g.Run).compile(List("tests/" + filename + ".scala", "tests/target.scala"))
   }
-  
-  @Test def testTest1 = runTest("Test1") 
+
+  @Test def testTest1 = runTest("Test1")
 }
