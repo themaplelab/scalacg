3a3fc866c6106e720a4be6315d751a57becd5afe
olhotak <olhotak@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-19 20:19:16 +0000

    implement basic compiler plugin that just prints annotations
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10051 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ src/callgraph/CallGraphPlugin.scala
@@ -0,0 +1,42 @@
+package callgraph
+
+import scala.tools.nsc
+import nsc.plugins.Plugin
+import scala.collection.immutable.List
+import nsc.Global
+import nsc.plugins.PluginComponent
+import nsc.Phase
+
+class CallGraphPlugin(val global: Global) extends Plugin {
+	val name = "callgraph"
+			val description = "builds a call graph"
+			val components = List[PluginComponent](Component)
+
+			private object Component extends PluginComponent {
+		val global = CallGraphPlugin.this.global
+				import global._
+				val runsAfter = List[String]("refchecks") // TODO: is this the right place for the phase?
+				def newPhase(prevPhase: Phase) = new CallGraphPhase(prevPhase)
+		val phaseName = CallGraphPlugin.this.name
+
+		class CallGraphPhase(prevPhase: Phase) extends StdPhase(prevPhase) {
+			def apply(unit: global.CompilationUnit) = {
+				for(tree <- unit.body) {
+					tree match {
+					case Apply(
+							Select(
+									Block(
+											List(annot),
+											receiver
+											),
+											methodName
+									), args) =>
+					println("noticed call site with annotation: "+annot)
+
+					case _ =>
+					}
+				}
+			}
+		}
+	}
+}
--- src/test/Suite.scala
+++ src/test/Suite.scala
@@ -7,6 +7,7 @@ class Suite {
    def runTest(filename: String) = {
       val settings = new nsc.Settings
       settings.d.value = "tests/classes"
+        settings.plugin.value = List("bin/callgraph-plugin.jar")
       val g = new nsc.Global(settings)
       new (g.Run).compile(List("tests/"+filename+".scala", "tests/target.scala"))
   }
