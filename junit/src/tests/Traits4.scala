9d2605dbfdec93466bad3dd0641719df8bc4af27
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-20 21:35:01 +0000

    added test
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10100 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/Traits4.scala
@@ -0,0 +1,19 @@
+package tests
+
+object Traits4 {
+  def main(args: Array[String]) = {
+	  { "Y.foo"; (new Y with X1)}.foo();
+	  { "Y.foo"; (new Y with X2)}.foo();
+  }
+
+  trait X1 { 
+	  def foo();
+  }
+  trait X2 { 
+	  def foo();
+  }
+  
+  trait Y {
+	  @target("Y.foo") def foo(): Unit = { }
+  }
+}
