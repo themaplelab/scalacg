818cf708e31d72c0cef544e8b2039e27c42a1f53
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-21 15:41:48 +0000

    added test
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10112 e628a048-d915-0410-9aa0-f7087ab87138

--- src/test/Suite.scala
+++ src/test/Suite.scala
@@ -31,6 +31,7 @@ class Suite {
   @Test def testTraits2 = runTest("Traits2")
   @Test def testTraits3 = runTest("Traits3")
   @Test def testTraits4 = runTest("Traits4")
+  @Test def testTraits5 = runTest("Traits5")
 
   @Test def testOverriding1 = runTest("Overriding1")
   @Test def testOverloading = runTest("Overloading")
