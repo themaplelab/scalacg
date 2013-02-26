24aa0d3a820a9a662ec16bb37b3aa54cb3fc4e5f
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-02-26 16:49:06 +0000

    a simple example that shows how analyzing Scala source code yields more precise results than analyzing bytecode (given that we have the SHA analysis).
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10160 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/Simple.scala
@@ -0,0 +1,28 @@
+package tests
+
+
+object SimpleTraits {
+  def main(args: Array[String]) = {
+    
+  }
+
+  trait A {
+    @target("A.foo") def foo();
+    
+    @target("A.bar") def bar(): Unit = {
+      { "B.foo"; this }.foo();
+      println("A.bar()");
+    }
+  }
+  
+  class B extends A {
+    @target("B.foo") def foo() : Unit = { println("B.foo()") }
+  }
+  
+  class C extends A {
+    @target("C.foo") def foo() : Unit = { println("C.foo()") }
+    @target("C.bar") override def bar() : Unit = {
+      
+    }
+  }
+}
