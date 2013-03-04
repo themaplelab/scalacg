8aad157ad5efcd71c6c2b354d65ff87f777a8b02
olhotak <olhotak@e628a048-d915-0410-9aa0-f7087ab87138>
2013-03-04 16:18:04 +0000

    fix call site annotation format in Traits9
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10241 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/Traits9.scala
+++ junit/src/tests/Traits9.scala
@@ -3,7 +3,7 @@ package tests
 object Traits9 {
     trait A {
       def foo();
-      def bar(){ { { "B.foo"; "C.foo" }; this}.foo(); } // can only call B.foo() or C.foo() --- the run-time type of this might be "C" because there is a super-call to A.bar()
+      def bar(){ { "B.foo"; "C.foo"; this}.foo(); } // can only call B.foo() or C.foo() --- the run-time type of this might be "C" because there is a super-call to A.bar()
     }
 
     class B extends A {
