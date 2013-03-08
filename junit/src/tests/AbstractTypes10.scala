3974ea56d379afa20195c19aa0f537381d7ad77e
mschaefer <mschaefer@e628a048-d915-0410-9aa0-f7087ab87138>
2013-03-08 08:46:17 +0000

    Attempt at example where taking upper bound of type member is too imprecise.
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10262 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/AbstractTypes10.scala
@@ -0,0 +1,20 @@
+package tests
+
+trait A {
+  type T
+  val field : T
+  def foo() = { "C.hashCode"; field }.hashCode()
+}
+
+class B extends A {
+  type T = C
+  val field : T = new C()
+}
+
+class C {
+  @target("C.hashCode") override def hashCode() = 42
+}
+
+class D {
+  @target("D.hashCode") override def hashCode() = 23
+}
