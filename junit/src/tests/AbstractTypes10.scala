b19cd3abf7ad9681413c450fca5d562040d7d1f6
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-03-08 17:21:00 +0000

    wrapped inside a type AbstractTypes10 to avoid polluting the global namespace
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10267 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/AbstractTypes10.scala
+++ junit/src/tests/AbstractTypes10.scala
@@ -1,20 +1,22 @@
 package tests
 
-trait A {
-  type T
-  val field : T
-  def foo() = { "C.hashCode"; field }.hashCode()
-}
-
-class B extends A {
-  type T = C
-  val field : T = new C()
-}
-
-class C {
-  @target("C.hashCode") override def hashCode() = 42
-}
-
-class D {
-  @target("D.hashCode") override def hashCode() = 23
+object AbstractTypes10 {
+	trait A {
+	  type T
+	  val field : T
+	  def foo() = { "C.hashCode"; field }.hashCode()
+	}
+	
+	class B extends A {
+	  type T = C
+	  val field : T = new C()
+	}
+	
+	class C {
+	  @target("C.hashCode") override def hashCode() = 42
+	}
+	
+	class D {
+	  @target("D.hashCode") override def hashCode() = 23
+	}
 }
