d458a6ddd23973257fed76bf6e10d57c50bfb955
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-03-13 22:09:29 +0000

    moved the annotation to the main src tree
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10280 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/annotation/targetmethod.java
+++
@@ -1,8 +0,0 @@
-package annotation;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface targetmethod {
-	public int value() default 0;
-}
