ea9aaaead5a1260fb09b93e6cf8cd9792af8070f
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-04-11 14:18:27 +0000

    another simple test for implicits
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10462 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/Implicits2.scala
@@ -0,0 +1,27 @@
+package tests
+
+object Implicits3 {
+  class B(n : Int){
+    def get : Int = n;
+    override def toString() = "B[" + n + "]";
+  } 
+  // implicit defined in "companion object"
+  object B {
+	@target("B2A") implicit def BtoA(b : B):A = new A(b.get*b.get);    
+  }
+ 
+  
+  
+  class A(p : Int) {
+    override def toString() = "A[" + p + "]";
+  }
+   
+  def printA(a : A){ 
+    println(a)
+  }
+  
+  def main(args: Array[String]) {
+     val b = new B(7);
+     printA(b); // prints "A[49]"
+  }
+}
