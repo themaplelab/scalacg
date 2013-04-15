813b6cedfe5942013b11edb02e15f0cf89985934
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-04-15 17:01:09 +0000

    added test
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10500 e628a048-d915-0410-9aa0-f7087ab87138

--- src/test/Suite.scala
+++ src/test/Suite.scala
@@ -62,6 +62,7 @@ class Suite {
   @Test def testPathTypes4 = runTest("PathTypes4") 
   @Test def testPathTypes5 = runTest("PathTypes5") 
   @Test def testPathTypes6 = runTest("PathTypes6") 
+  @Test def testPathTypes7 = runTest("PathTypes7") 
   
   @Test def testAbstractTypes1 = runTest("AbstractTypes1") 
   @Test def testAbstractTypes2 = runTest("AbstractTypes2") 
