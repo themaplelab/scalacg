636431fc3a1eb6094562884692254413682fa03e
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-01 14:16:25 +0000

    extended the test
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10752 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/Apply1.scala
+++ junit/src/tests/Apply1.scala
@@ -3,12 +3,20 @@ package tests
 object Apply1 {
 
   def main(args: Array[String]) = {
-    val v = AddFive()(1)
-    println(v)
+    val v1 = AddFive()(1) // calls generated constructor, then calls apply()
+    println(v1)
+    
+    val v2 = AddFive() // calls generated constructor
+    v2(1) // calls apply()
+    println(v2)
+    
+    val v3 = AddFive() 
+    v3.apply(1) // calls apply()
+    println(v3)
   }
 
   case class AddFive {
     def apply(x: Int) = x + 5
   }
-
+  
 }
