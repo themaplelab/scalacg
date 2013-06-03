9d7182f7ada8afadc66482e3661b5b5195635a54
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-03 15:51:04 +0000

    adding test case for foreach
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10757 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/Foreach1.scala
@@ -0,0 +1,27 @@
+package tests
+
+object Foreach1 {
+
+  def main(args: Array[String]): Unit = {
+    
+    // This calls A.apply
+    for(v <-  0 to 10) {
+      A(v)
+    }
+    
+    // This calls B.apply
+    for(v <-  0 to 10) {
+      B(v)
+    }
+
+  }
+
+  object A {
+    def apply(x: Int) = { "A" + x }
+  }
+
+  object B {
+    def apply(x: Int) = { "B" + x }
+  }
+
+}
