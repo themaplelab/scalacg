36a7593d6680ebbd3b2e9d4383834b499810679b
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-10 21:19:34 +0000

    fixed assertions
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10838 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/Operator1.scala
+++ junit/src/tests/Operator1.scala
@@ -1,19 +1,20 @@
 package tests
 
 import callgraph.annotation.target
+import callgraph.annotation.invocations
 
 object Operator1 {
 
   class C(x : Int) {    
-    def :: (y : Int): C = new C(x * y)
-    @target("::") override def toString() = x.toString;
+    @target("::") def :: (y : Int): C = new C(x * y)
+    override def toString() = x.toString;
   }
   
+  @invocations("ADD ASSERTION FOR THE CALL ON LINE 15")
   def main(args: Array[String]): Unit = {
      val c = new C(10);
      val d = 10 :: c; // how to place an assertion here??
      val e =  { "::"; d}.::(10);
      println(e);
   }
-
 }
