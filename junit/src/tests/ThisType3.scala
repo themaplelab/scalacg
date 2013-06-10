6a7bd299e134ced0302fb289c7dc6fb1d9a03f85
ftip <ftip@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-10 16:39:42 +0000

    added @invocations annotation
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10827 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/ThisType3.scala
+++ junit/src/tests/ThisType3.scala
@@ -1,6 +1,7 @@
 package tests
 
 import callgraph.annotation.target
+import callgraph.annotation.invocations
 
 object ThisType3 {
     class A {
@@ -8,13 +9,14 @@ object ThisType3 {
        @target("A.zap") def zap() { println("A.zap"); }
     }
     class B extends A {
-      override def zip(x : this.type) : Unit = { { "B.zap"; x}.zap(); } 
+      @target("B.zip") override def zip(x : this.type) : Unit = { { "B.zap"; x}.zap(); } 
       @target("B.zap") override def zap()  { println("B.zap"); }
     }
     
+    @invocations("19: A.zip")
     def main(args: Array[String]) = {
       val x : A = new B();
       val _ = new A();
-      x.zip(x);
+      x.zip(x); // cannot place annotation here because the argument has type "this.type"
     }  
 } 
