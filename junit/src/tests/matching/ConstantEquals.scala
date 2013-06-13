2c3fcc02fea6625942116cef5eae96a9c9e45ef7
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-13 16:30:46 +0000

    Karim: The call below will not be in the call graph because Nil is not instantiated in the application. Our analysis only knows about the types instantiated in the code fed to the compiler (i.e., the application). // @invocations("14: <unannotated> scala.collection.immutable.Nil: equals()")
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10879 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/matching/ConstantEquals.scala
+++ junit/src/tests/matching/ConstantEquals.scala
@@ -1,18 +1,19 @@
 package tests.matching
 
-import callgraph.annotation.target
 import callgraph.annotation.invocations
 
 object ConstantEquals {
 
-   /**
-    * Testing extractors
-    */
-   @invocations("14: <unannotated> scala.collection.immutable.Nil: equals()")
-   def main(args: Array[String]) {
+  /**
+   * Testing extractors
+   */
+  // Karim: The call below will not be in the call graph because Nil is not instantiated in the application.
+  // Our analysis only knows about the types instantiated in the code fed to the compiler (i.e., the application).
+  // @invocations("14: <unannotated> scala.collection.immutable.Nil: equals()")
+  def main(args: Array[String]) {
     Nil match {
       case Nil => println("right")
       case _ => println("wrong")
     }
   }
- }
+}
