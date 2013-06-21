b067acd24e1da3b9d985ed161538f9ecebc5105a
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-21 18:10:46 +0000

    changing CaseClass to use Lit.value
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@10964 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/matching/CaseClass.scala
+++ junit/src/tests/matching/CaseClass.scala
@@ -8,31 +8,29 @@ object CaseClass {
   /**
    * Testing pattern matching on case class
    */
-  @invocations("27: <unannotated> tests.matching.CaseClass.Lit: <init>(value: Boolean)",
-    "28: <unannotated> scala.MatchError: <init>(obj: Any)",
+  @invocations("25: <unannotated> tests.matching.CaseClass.Lit: <init>(value: Boolean)",
+    "26: <unannotated> scala.MatchError: <init>(obj: Any)",
     //"29: <unannotated> java.lang.Object: ne(x$1: AnyRef)", // those won't appear b/c java.lang.Object is not instantiated in the application.
+    "27: <unannotated> scala.Any: isInstanceOf()",
+    "27: <unannotated> scala.Any: asInstanceOf()", // These two calls are in not in the call graph because there's no
+    "27: <unannotated> tests.matching.CaseClass.Lit: value()", // call site for Lit.value in main, though there's one in the intermediate code! Weird.
+    "28: <unannotated> scala.Any: asInstanceOf()",
+    "28: <unannotated> scala.Any: isInstanceOf()",
+    "28: <unannotated> tests.matching.CaseClass.Var: name()",
+    "29: <unannotated> scala.Any: asInstanceOf()",
     "29: <unannotated> scala.Any: isInstanceOf()",
-    /*"29: <unannotated> scala.Any: asInstanceOf()", // These two calls are in not in the call graph because there's no
-    "29: <unannotated> tests.matching.CaseClass.Lit: value()",*/ // call site for Lit.value in main, though there's one in the intermediate code! Weird.
-    //"30: <unannotated> java.lang.Object: ne(x$1: AnyRef)",
-    "30: <unannotated> scala.Any: asInstanceOf()",
-    "30: <unannotated> scala.Any: isInstanceOf()",
-    "30: <unannotated> tests.matching.CaseClass.Var: name()",
-    //"31: <unannotated> java.lang.Object: ne(x$1: AnyRef)",
-    "31: <unannotated> scala.Any: asInstanceOf()",
-    "31: <unannotated> scala.Any: isInstanceOf()",
-    "31: <unannotated> tests.matching.CaseClass.And: left()",
-    "31: <unannotated> tests.matching.CaseClass.And: right()")
+    "29: <unannotated> tests.matching.CaseClass.And: left()",
+    "29: <unannotated> tests.matching.CaseClass.And: right()")
   def main(args: Array[String]) {
     val e: Expr = Lit(value = true)
     e match {
-      case Lit(v) => { "foo"; this }.foo()
+      case Lit(v) => { "foo"; this }.foo(v)
       case Var(z) => Console.println(z)
       case And(l, r) => Console.println(l + ", " + r)
     }
   }
 
-  @target("foo") def foo() {
+  @target("foo") def foo(v: Boolean) {
     Console.println("foo")
   }
 
