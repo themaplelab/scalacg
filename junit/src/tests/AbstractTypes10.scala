afdadd4f223e1e84b47eebbe10fc9662bb076834
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-06-26 14:51:00 +0000

    added a comment
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11014 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/AbstractTypes10.scala
+++ junit/src/tests/AbstractTypes10.scala
@@ -23,8 +23,11 @@ object AbstractTypes10 {
   
   // FT: inconsistent results? Why does our analysis consider D.hashCode a
   // reachable method, but without making it a target of the call on line 11?
+  //
+  // KA: this method will be reachable because it could be called back from the library. It has nothing to do with the
+  // call on line 11.
   class D {
-    @notreachable @target("D.hashCode") override def hashCode() = 23
+    @reachable @target("D.hashCode") override def hashCode() = 23
   }
   def main(args: Array[String]): Unit = {
     { "A.foo"; new B()}.foo
