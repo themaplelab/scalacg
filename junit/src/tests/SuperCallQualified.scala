8f6e69d23da12a11d5f4733e852f4654ae6a6e2b
mrapopor <mrapopor@e628a048-d915-0410-9aa0-f7087ab87138>
2013-07-04 00:25:27 +0000

    missing tests added
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11046 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/SuperCallQualified.scala
@@ -0,0 +1,28 @@
+package tests
+
+import callgraph.annotation.{ target, invocations }
+
+class Trallala {
+  def bam() = "Trallala.bam"
+}
+
+trait Lalala {
+  @target("Lalala.bam")
+  def bam() = "Lalala.bam"
+}
+
+object SuperCallQualified extends Trallala with Lalala {
+
+  override def bam() = "SuperCallQualified.bam"
+
+  class B {
+    @invocations("21: Lalala.bam")
+    def m() {
+      SuperCallQualified.super[Lalala].bam()
+    }
+  }
+
+  def main(args: Array[String]) {
+    new B().m() // == "Lalala.bam"
+  }
+}
