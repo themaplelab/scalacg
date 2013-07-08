c00c71b064ff4ec405c241f4c78044b93ab68aae
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-07-08 17:20:35 +0000

    changed call to library to conform to our new implementation
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11059 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/Match2.scala
+++ junit/src/tests/Match2.scala
@@ -5,7 +5,7 @@ import callgraph.annotation.invocations
 object Match2 {
 
   @invocations("11: <unannotated> tests.Match2.X: <init>(a: Int)",
-               "12: <unannotated> java.lang.Object: ne(x$1: AnyRef)", 
+               "12: <unannotated> java.lang.Object: ne(x$1: Object)", 
                "13: <unannotated> tests.Match2.X: a()")
   def main(args: Array[String]) = {
     val e = X(1)
