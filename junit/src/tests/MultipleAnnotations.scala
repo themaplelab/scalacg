6805e2f0d7d77bcea73ee20f98c6da7fa12eab33
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-07-08 21:34:35 +0000

    adding necessary @reachable annotations to handle closures
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11067 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/MultipleAnnotations.scala
+++ junit/src/tests/MultipleAnnotations.scala
@@ -2,11 +2,15 @@ package tests
 
 import callgraph.annotation.invocations
 import callgraph.annotation.target
+import callgraph.annotation.reachable
 
 object MultipleAnnotations {
 
-  @invocations("13: A.apply")
-  @invocations("18: B.apply")
+  /*
+   * Karim: see Reachable1
+   * @invocations("13: A.apply")
+   * @invocations("18: B.apply")
+   */
   def main(args: Array[String]) = {
     // This calls A.apply
     for (v <- 0 to 10) {
@@ -21,11 +25,13 @@ object MultipleAnnotations {
 
   object A {
     @target("A.apply")
+    @reachable
     def apply(x: Int) = { "A" + x }
   }
 
   object B {
     @target("B.apply")
+    @reachable
     def apply(x: Int) = { "B" + x }
   }
 
