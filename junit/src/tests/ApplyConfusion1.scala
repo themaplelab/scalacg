47bd564e1d95c666cbe012205c0816f58215689d
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-07-08 21:42:34 +0000

    adjusting the @invocations annotations because we now do our analysis after uncurry not refchecks
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11069 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/ApplyConfusion1.scala
+++ junit/src/tests/ApplyConfusion1.scala
@@ -2,24 +2,29 @@ package tests
 
 import scala.collection.immutable.LinearSeq
 import callgraph.annotation.invocations
+import callgraph.annotation.reachable
 
 object ApplyConfusion1 {
 
   class A extends (Int => String) {
+    @reachable
     def apply(m: Int) = "hello";
   }
 
   class B {
+    @reachable
     def foo(j: Int) = "goodbye";
-    
-    @invocations("16: tests.ApplyConfusion1.B: foo(j: Int)")
+
+    // Karim: the closures issue.
+    // @invocations("16: tests.ApplyConfusion1.B: foo(j: Int)")
     def bar(z: (Int => String)) = z(3); // calls scala/Function1.apply:(Ljava/lang/Object;)Ljava/lang/Object;
-    
+
     def zap() = bar(foo);
   }
 
   // This gets dispatched to A.apply not Function1.apply
-  @invocations("25: <unannotated> tests.ApplyConfusion1.A: apply(m: Int)")
+  // Karim: the closures issue.
+  // @invocations("25: <unannotated> tests.ApplyConfusion1.A: apply(m: Int)")
   def main(args: Array[String]): Unit = {
     val x: (Int => String) = new A();
     val y = x(2);
--- junit/src/tests/Foreach2.scala
+++ junit/src/tests/Foreach2.scala
@@ -1,19 +1,23 @@
 package tests
 
 import callgraph.annotation.invocations
+import callgraph.annotation.reachable
 
 /**
- * A test case that examines how methods calls within a foreach are lifted. It looks like in some cases, the body of 
- * a foreach is lifted to an anonymous apply function that is created later on in the compiler. So we don't see it 
+ * A test case that examines how methods calls within a foreach are lifted. It looks like in some cases, the body of
+ * a foreach is lifted to an anonymous apply function that is created later on in the compiler. So we don't see it
  * at refchecks.
  */
 object Foreach2 {
 
-  @invocations("18: <unannotated> scala.Tuple2: <init>(_1: T1,_2: T2)", 
+  /*
+   * Karim: commenting out these invocations because we can't test for them after the body of foreach gets desugared
+   * as a closure. Using @reachable instead for B.apply.
+   * @invocations("18: <unannotated> scala.Tuple2: <init>(_1: T1,_2: T2)", 
                "19: <unannotated> scala.MatchError: <init>(obj: Any)", 
                "20: <unannotated> tests.Foreach2.B: <init>()", 
                "21: <unannotated> tests.Foreach2.B: apply(i: Int)"
-               )
+               )*/
   def main(args: Array[String]): Unit = {
     val values = List[Int](1, 2, 3, 4) map (e => (e, e))
     for ((v, e) <- values) {
@@ -25,8 +29,9 @@ object Foreach2 {
   abstract class A {
     def apply(i: Int): String
   }
-  
+
   class B extends A {
+    @reachable
     def apply(i: Int) = {
       "" + i
     }
