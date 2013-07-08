a7b46e1788f8157d3e1fa7082069db739c44830b
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-07-08 21:25:59 +0000

    adding necessary @reachable annotations
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11063 e628a048-d915-0410-9aa0-f7087ab87138

--- junit/src/tests/Closures1.scala
+++ junit/src/tests/Closures1.scala
@@ -1,6 +1,7 @@
 package tests
 
 import callgraph.annotation.target
+import callgraph.annotation.reachable
 
 object Closures1 {
 
@@ -8,24 +9,29 @@ object Closures1 {
 
     // how to specify the expected call graph result for function calls
     // using block syntax such as   { "bar"; bar }(foo1) causes syntax errors..
-    { "bar"; this}.bar(foo1);
-    { "bar"; this}.bar(foo2);
+    { "bar"; this }.bar(foo1);
+    { "bar"; this }.bar(foo2);
 
-    { "baz"; this}.baz(zap1);
-    { "baz"; this}.baz(zap2);
-    
-     { "FORCE_TEST_FAILURE"; this}.fail(); // force test to fail until we have @invocations assertions on the calls in bar and baz
+    { "baz"; this }.baz(zap1);
+    { "baz"; this }.baz(zap2);
   }
+
   
-  def fail(){}
+  /*
+   * Karim: the following methods are reachable because they will appear in the apply method of the Function0/1 objects
+   * that will be created after desugaring closures.
+   */
+  @reachable def foo1(): Unit = println("foo1")
+  @reachable def foo2(): Unit = println("foo2")
+  @reachable def zap1(i: Int): Unit = println("zap1")
+  @reachable def zap2(i: Int): Unit = println("zap2")
 
-  @target("foo1") def foo1(): Unit = { println("foo1"); }
-  @target("foo2") def foo2(): Unit = { println("foo2"); }
-
-  @target("zap1") def zap1(i: Int): Unit = { println("zap1"); }
-  @target("zap2") def zap2(i: Int): Unit = { println("zap2"); }
-
-  @target("bar") def bar(z: () => Unit): Unit = { z(); }
-  @target("baz") def baz(z: (Int) => Unit): Unit = { z(3); }
+  @reachable
+  @target("bar") 
+  def bar(z: () => Unit): Unit = { z(); }
+  
+  @reachable
+  @target("baz") 
+  def baz(z: (Int) => Unit): Unit = { z(3); }
 
 }
