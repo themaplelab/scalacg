8fe90a6e504971b6ad9a88372564e95ba1bd6971
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2013-11-29 21:12:19 +0000

    test case for calling apply(Object) methods
    git-svn-id: https://svn-plg.uwaterloo.ca/svn/code/scalacg@11775 e628a048-d915-0410-9aa0-f7087ab87138

---
+++ junit/src/tests/tca/wala/Apply.scala
@@ -0,0 +1,30 @@
+package tests.tca.wala
+
+object Apply {
+
+  def main(args: Array[String]): Unit = {
+    val t: Type = TTop
+    val o = O.lookupByType(t)
+    val a = new API.Writer().typeToXML(t, t => t)
+  }
+
+  object API {
+    class Writer {
+      def typeToXML(typ: Type, widen: Type => Type) {
+        widen(typ)
+      }
+    }
+  }
+
+  sealed abstract class Type
+  case object TTop extends Type
+  class TUnion(val types: Set[Type]) extends Type
+
+  object O {
+    def lookupByType(typ: Type): Type = typ match {
+      case tu: TUnion => tu.types.map(lookupByType).head
+      case _ => TTop
+    }
+  }
+
+}
